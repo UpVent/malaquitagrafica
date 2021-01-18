4c36d2ad8355e93fdc848eeec85a22ada635f3f8
VentGrey <omarpurataf@gmail.com>
2021-01-18 04:13:10 -0600

    Pruebas con notificador de navegadores

---
+++ js/browser-update.js
@@ -0,0 +1,24 @@
+var $buoop = {
+    required:{
+        e:0,f:0,o:0,s:0,c:0
+    },
+    insecure:true,
+    reminder: 24,
+    reminderClosed: 10,
+    unsupported:true,
+    style:"bottom",
+    test: true,
+    api:2021.01
+};
+
+function $buo_f() {
+ var e = document.createElement("script");
+ e.src = "https://browser-update.org/update.min.js";
+ document.body.appendChild(e);
+};
+
+try {
+    document.addEventListener("DOMContentLoaded", $buo_f,false)
+} catch(e){
+    window.attachEvent("onload", $buo_f)
+}
